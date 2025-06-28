635ef93fa83f0163715248f2ad23d25d9a5418d4
TT <112479728+634287319@users.noreply.github.com>
2025-06-28 21:06:01 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,22 +1,19 @@
-#!/bin/bash
-set -e
+# 下载二进制
+wget -O /usr/local/x-ui-linux-amd64.tar.gz https://github.com/634287319/3x-ui/releases/latest/download/x-ui-linux-amd64.tar.gz
 
-# 1. 安装依赖
-apt update -y && apt install -y curl wget tar
-
-# 2. 下载并解压你的 x-ui 版本
-wget -O /usr/local/x-ui-linux-amd64.tar.gz https://your.custom.domain/path/to/tar.gz
+# 解压和部署
 cd /usr/local
 tar zxvf x-ui-linux-amd64.tar.gz
 cd x-ui
 chmod +x x-ui
 cp -f x-ui.service /etc/systemd/system/
+
+# 设置默认账号
+/usr/local/x-ui/x-ui setting -username admin -password admin123 -port 54321 -webBasePath webxyz
+
+# 启动服务
 systemctl daemon-reload
 systemctl enable x-ui
 systemctl start x-ui
 
-# 3. 设置默认账号密码等
-/usr/local/x-ui/x-ui setting -username admin -password admin123 -port 54321 -webBasePath web123
-
-# 4. 输出访问信息
-echo "访问地址：http://$(curl -s https://api.ipify.org):54321/web123"
+echo "访问地址: http://$(curl -s https://api.ipify.org):54321/webxyz"
